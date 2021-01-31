ff39f5be21f6045d5e681b89438c138e5b2c9d2d
Foysal Ahmed Emon <emon535@gmail.com>
2021-01-31 14:46:51 +0600

    Project Init

---
+++ app/src/main/java/com/emon535/ju_direcroty/MainActivity.java
@@ -0,0 +1,20 @@
+package com.emon535.ju_direcroty;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static  void main(){
+        System.out.println("Something interesting in android is happening");
+    }
+}
